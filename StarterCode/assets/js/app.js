0d60c2e46c4a38547da2ec1cef5bd6f785d05552
cjvilla <CelineVillareal2020@northwestern.edu>
2021-04-24 14:19:36 -0500

    added makeResponsive func

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,33 +1,11 @@
-// @TODO: YOUR CODE HERE!
-var svgWidth = 960;
-var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-//creating SVG wrapper 
-var svg = d3
-    .select('.article')
-    .append('svg')
-    .attr('width', svgWidth)
-    .attr('height', svgHeight);
-
-var chartGroup = svg.append('g')
-    .attr('transofrm', `translate(${margin.left}), ${margin.top} `);
-
-// id,state,abbr,poverty,povertyMoe,age,
-// ageMoe,income,incomeMoe,healthcare,
-//healthcareLow,healthcareHigh,obesity,
-//obesityLow,obesityHigh,smokes,smokesLow,
-//smokesHigh
-
-var chosenXAxis = 'healthcare'
-
-function xScale(data, chosenXAxis)
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+  // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+  // clear svg is not empty
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+}
